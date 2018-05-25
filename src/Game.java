6371438f8d92d6fcfb7eefc1eb9985f7ce56b158
Mal Einstein <patrickmalachi789@gmail.com>
2018-05-25 11:32:14 -0400

    This is the code that opens the Ludo board to the jre window

---
+++ src/Game.java
@@ -0,0 +1,78 @@
+public class Game
+{
+  private Grid grid;
+  private int userRow;
+  private int msElapsed;
+  private int timesGet;
+  private int timesAvoid;
+  
+  public Game()
+  {
+    grid = new Grid(258, 259, "Ludo_board.jpg");
+    userRow = 0;
+    msElapsed = 0;
+    timesGet = 0;
+    timesAvoid = 0;
+    updateTitle();
+    grid.setImage(new Location(userRow, 7), "user.gif");
+  }
+  
+  public void play()
+  {
+    while (!isGameOver())
+    {
+      grid.pause(100);
+      handleKeyPress();
+      if (msElapsed % 300 == 0)
+      {
+        scrollLeft();
+        populateRightEdge();
+      }
+      updateTitle();
+      msElapsed += 100;
+    }
+  }
+  
+  public void handleKeyPress()
+  {
+	  int key = grid.checkLastKeyPressed();
+  }
+  
+  public void populateRightEdge()
+  {
+  }
+  
+  public void scrollLeft()
+  {
+  }
+  
+  public void handleCollision(Location loc)
+  {
+  }
+  
+  public int getScore()
+  {
+    return 0;
+  }
+  
+  public void updateTitle()
+  {
+    grid.setTitle("Game:  " + getScore());
+  }
+  
+  public boolean isGameOver()
+  {
+    return false;
+  }
+  
+  public static void test()
+  {
+    Game game = new Game();
+    game.play();
+  }
+  
+  public static void main(String[] args)
+  {
+    test();
+  }
+}
